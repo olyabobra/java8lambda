bfe6166caf27ace2abd5afd32fdedd9bd2858a5f
Olya Bobra <olyabobra@gmail.com>
2014-10-26 19:32:02 +0000

    Adding solution for exercise 1.b

---
+++ src/main/edu/bobra/lambdas/chapter3/ArtistFormatter.java
@@ -0,0 +1,15 @@
+package edu.bobra.lambdas.chapter3;
+
+import java.util.function.Function;
+
+import edu.bobra.lambdas.domain.Artist;
+
+public class ArtistFormatter implements Function<Artist, String>{
+
+	@Override
+	public String apply(Artist artist) {
+		return String.format("%s (%s)", artist.getName(),
+				artist.getorigin());
+	}
+
+}
---
+++ src/test/edu/bobra/lambdas/domain/ArtistFixture.java
@@ -0,0 +1,5 @@
+package edu.bobra.lambdas.domain;
+
+public class ArtistFixture {
+
+}
